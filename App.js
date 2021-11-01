17d7eb4920515a7ec240c4672873779d3438648a
wallace.silva <wallace.silva@acpgroup.com.br>
2021-10-31 23:50:17 -0300

    commit

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Start from './src/pages/Start'
+//import StateProvider from './src/contexts/ContextProvider';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const Stack = createNativeStackNavigator();
+let store = createStore();
+
+import Start from './src/pages/Start';
+
 
 export default function App() {
+
+
+  
+  const initialState = { player: 0, computer: 0};
+  
+  const reducer = (state, action)=> {
+    switch ( action.type) {
+      case "modifyPlayer":
+        return {
+          ...state, player
+        }
+        break;
+    
+      default:
+        break;
+    }
+  }
+
   return (
-   <Start/>
-  );
+    <NavigationContainer >
+      <Stack.Navigator initialRouteName={rotaInicio}>
+        <Stack.Screen name="Start" 
+                      component={Start}
+                    options={{
+                      title: "",
+                      headerShown: false
+                     }}
+                      />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+
+    );
 }
 
 const styles = StyleSheet.create({
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,4 +1,4 @@
-import React, {createContext, useContext, useReducer} from 'react';
+/* import React, {createContext, useContext, useReducer} from 'react';
 
 
 export const StateContext = createContext();
@@ -9,5 +9,73 @@ export const SatateProvider = ({reducerm initialState, children}) => (
 </StateContext.Provider>
 );
 
-export const useState = () => useContext(StateContext);
+export const useState = () => useContext(StateContext); */
+
+
+import React, {createContext, useReducer} from 'react'
+import users from '../data/users'
+
+const initialState = { player: 0, computer: 0 } //estado inicial
+const StateContext = createContext({})
+
+const actions = {
+    updatePlayer(state,action){
+        const updated = action.payload
+        return {
+            ...state,
+            player: updated.value
+        }
+    },
+
+/*     createUser(state, action){
+        const user = action.payload
+        user.id =Math.random()
+        return {
+            ...state,
+            users: [...state.users, user],
+
+        } 
+    },*/
+   /*  deleteUser(state, action) {
+        const user = action.payload
+        return {
+             ...state,
+            users: state.users.filter( u => u.id !== user.id)
+
+    } */
+
+}
+
+
+
+
+
+
+//objetivo do reducer é evoluir o estado
+export const ContextProvider = props => {
+    
+    function reducer(state, action){//state representa a lista de usuario em sua ultima versao
+       // console.warn(action)
+        const fn = actions[action.type]
+
+       return fn ? fn(state, action) : state
+
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState)  
+    return (
+        <UsersContext.Provider value={{
+           /* state: {
+                users
+            }
+            */
+           state, dispatch
+        }}>
+            {props.children}
+
+        </UsersContext.Provider>
+    )
+}
+
+export default UsersContext
 
